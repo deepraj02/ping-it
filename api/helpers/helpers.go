1cf817925e3e793997fcda11f474df2c1e2167d3
Deepraj Baidya <deeprajbaidya@gmail.com>
2024-07-02 22:40:27 +0530

    ✨ (feat): helpers(http & domain validation)

--- api/helpers/helpers.go
+++ api/helpers/helpers.go
@@ -1,1 +1,25 @@
 package helpers
+
+import "strings"
+
+func EnforceHTTP(url string) string {
+	if url[:4] != "http" {
+		return "http://" + url
+	}
+	return url
+}
+
+func RemoveDomainError(url string) bool {
+	if url == "localhost" {
+		return false
+	}
+	newUrl := strings.Replace(url, "http://", "", 1)
+	newUrl = strings.Replace(newUrl, "https://", "", 1)
+	newUrl = strings.Replace(newUrl, "www.", "", 1)
+	newUrl = strings.Split(newUrl, "/")[0]
+
+	if newUrl != "localhost" {
+		return true
+	}
+	return true
+}
