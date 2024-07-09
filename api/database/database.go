f1d457269d5eddf054d76b246c8978d1f5c0eff5
Deepraj Baidya <deeprajbaidya@gmail.com>
2024-07-09 23:27:36 +0530

    ✨ (feat): resolver and redis init for db storage

--- api/database/database.go
+++ api/database/database.go
@@ -1,1 +1,18 @@
 package database
+
+import (
+	"context"
+
+	"github.com/redis/go-redis/v9"
+)
+
+var Ctx = context.Background()
+
+func CreateClient(dbNo int) *redis.Client {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "db:6379",
+		Password: "",
+		DB:       dbNo,
+	})
+	return rdb
+}
--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -1,5 +1,26 @@
 package routes
 
+import (
+	"github.com/deepraj02/pingit/database"
+	"github.com/gofiber/fiber/v2"
+	"github.com/redis/go-redis/v9"
+)
 
+func ResolveURL(c *fiber.Ctx) error {
+	url := c.Params("url")
+	r := database.CreateClient(0)
+	defer r.Close()
+	value, err := r.Get(database.Ctx, url).Result()
+	if err == redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "URL Not Found"})
 
-func ResolveURL(){}
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Internal Server Error"})
+	}
+
+	rInr := database.CreateClient(1)
+	defer rInr.Close()
+	_ = rInr.Incr(database.Ctx, "counter")
+	return c.Redirect(value, 301)
+
+}
