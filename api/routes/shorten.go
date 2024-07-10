74395848998eb2971bf44a983f823b9a38a06477
Deepraj Baidya <deeprajbaidya@gmail.com>
2024-07-10 23:01:34 +0530

    ✨ (feat): custom short --wip

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -9,6 +9,7 @@ import (
 	"github.com/deepraj02/pingit/database"
 	"github.com/deepraj02/pingit/helpers"
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -62,6 +63,30 @@ func ShortenURL(c *fiber.Ctx) error {
 
 	// Enforce SSL, HTTPS
 	body.URL = helpers.EnforceHTTP(body.URL)
+
+	var id string
+	fmt.Println(id)
+	if body.CustomShort == "" {
+		id = uuid.New().String()[:6]
+
+	} else {
+		id = body.CustomShort
+	}
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	val, _ = r.Get(database.Ctx, id).Result()
+	if val != "" {
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{"error": "Short URL already exists"})
+	}
+	if body.Expiry == 0 {
+		body.Expiry = 24
+	}
+	e := r.Set(database.Ctx, id, body.URL, body.Expiry*3600*time.Second).Err()
+	if e != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Internal Server Error"})
+	}
+
 	r2.Decr(database.Ctx, c.IP())
 
 	return nil
