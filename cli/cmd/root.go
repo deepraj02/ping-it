13f037be32074e505c64fe14ff15cd011671c6ed
Deepraj Baidya <deeprajbaidya@gmail.com>
2024-07-14 17:13:45 +0530

    ♻️ (chore ): cli flags refactor

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -14,7 +14,9 @@ var rootCmd = &cobra.Command{
 	Use:   "pingit",
 	Short: "PingIt is an URL Shortener CLI tool.",
 
-	Long: `PingIt is a URL shortener service that provides both a command-line interface (CLI) and a web API, written in Go. This project is designed to offer a simple and efficient way to shorten URLs directly from your terminal or through HTTP requests.
+	Long: `
+	PingIt is a URL shortener service that provides both a command-line interface (CLI) and a web API, written in Go.
+	This project is designed to offer a simple and efficient way to shorten URLs directly from your terminal or through HTTP requests.
 	
 	Created with ❤️ by @Deepraj02
 	
@@ -26,8 +28,4 @@ func Execute() {
 	if err != nil {
 		os.Exit(1)
 	}
-}
-
-func init() {
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
